706ad71eb3147ecab6d657b5479ab404bae4b203
rainayangg <raina.yang.chn@gmail.com>
2025-08-08 16:22:20 +0000

    implement net.conn for komaconn

--- http2/koma_conn.go
+++ http2/koma_conn.go
@@ -7,8 +7,10 @@ import "C"
 
 import (
 	"fmt"
+	"net"
 	"os"
 	"syscall"
+	"time"
 
 	"golang.org/x/sys/unix"
 )
@@ -17,7 +19,19 @@ type KomaConn struct {
 	fd      C.int
 	file    *os.File
 	rawConn syscall.RawConn
-	from unix.Sockaddr
+	from    unix.Sockaddr
+}
+
+type KomaAddr struct {
+	fd C.int
+}
+
+func (a *KomaAddr) Network() string {
+	return "koma" // name your protocol/network here
+}
+
+func (a *KomaAddr) String() string {
+	return fmt.Sprintf("komaFD: %d", a.fd)
 }
 
 func NewKomaConn(fd C.int) (*KomaConn, error) {
@@ -39,8 +53,8 @@ func (k *KomaConn) Read(b []byte) (int, error) {
 	var err error
 	readErr := k.rawConn.Read(func(fd uintptr) bool {
 		n, _, _, k.from, err = unix.Recvmsg(int(fd), b, nil, 0)
-		if err == unix.EAGAIN || err == unix.EWOULDBLOCK { // --> I think returning false is necesary. 
-								// If we dont get data, we say the poller to again wait until the fd is available. This matches grpc expected behavior
+		if err == unix.EAGAIN || err == unix.EWOULDBLOCK { // --> I think returning false is necesary.
+			// If we dont get data, we say the poller to again wait until the fd is available. This matches grpc expected behavior
 			return false
 		}
 		return true
@@ -48,7 +62,7 @@ func (k *KomaConn) Read(b []byte) (int, error) {
 	if readErr != nil {
 		return 0, readErr
 	}
-	
+
 	return n, err
 }
 
@@ -56,6 +70,7 @@ func (k *KomaConn) Write(b []byte) (int, error) {
 	var n int
 	var err error
 
+	// TODO: change to sendmsgN in the future to prevent n always being 0.
 	writeErr := k.rawConn.Write(func(fd uintptr) bool {
 		err = unix.Sendmsg(int(fd), b, nil, k.from, 0)
 		if err == unix.EAGAIN {
@@ -69,4 +84,55 @@ func (k *KomaConn) Write(b []byte) (int, error) {
 	}
 
 	return n, err
+}
+
+// functions required to implement net.conn interface
+// the following are heavily copy-pasted from the net.conn implementation in src/net/net.go
+func (k *KomaConn) ok() bool { return k != nil && k.fd != 0 }
+
+// Close closes the connection.
+func (k *KomaConn) Close() error {
+	if !k.ok() {
+		return syscall.EINVAL
+	}
+	err := k.file.Close()
+	return err
+}
+
+// LocalAddr returns the local network address.
+// The Addr returned is shared by all invocations of LocalAddr, so
+// do not modify it.
+func (k *KomaConn) LocalAddr() net.Addr {
+	return &KomaAddr{fd: k.fd}
+}
+
+// RemoteAddr returns the remote network address.
+// The Addr returned is shared by all invocations of RemoteAddr, so
+// do not modify it.
+func (k *KomaConn) RemoteAddr() net.Addr {
+	if !k.ok() {
+		return nil
+	}
+	return &KomaAddr{fd: 0}
+}
+
+// SetDeadline is a stub. Not implemented yet.
+// TODO: check if its necessary.
+func (k *KomaConn) SetDeadline(t time.Time) error {
+	return nil
+}
+
+// SetReadDeadline is a stub.
+func (k *KomaConn) SetReadDeadline(t time.Time) error {
+	return nil
+}
+
+// SetWriteDeadline is a stub.
+func (k *KomaConn) SetWriteDeadline(t time.Time) error {
+	return nil
+}
+
+// SyscallConn exposes the underlying syscall.RawConn.
+func (k *KomaConn) SyscallConn() (syscall.RawConn, error) {
+	return k.rawConn, nil
 }
